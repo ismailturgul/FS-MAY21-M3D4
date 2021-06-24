50c2a8d58e0ef1c99f6f5145b9f7364d7e9cb5ba
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-06-24 15:48:15 +0200

    M3D4 Homework
    Added Button : ShoppingCart & ignore
    Added new Page : Shoppingcart
    Added NavBar and linked all things

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-
+const btnAddToCart = document.querySelector(".addToCart")
 const mainRow = document.querySelector(".main-row");
+const favRow = document.querySelector(".favorite.row")
 
 const getBooks = function(){
     fetch("https://striveschool-api.herokuapp.com/books").then(function(response){
         console.log(response);
         return response.json();
     }).then(function(data){
-        console.log(data.title)
+        console.log(data)
         displayHTM(data);
     })
 };
-getBooks([]) 
 
 const displayHTM = (datas) => {
     datas.forEach((data) => {
       // const img {medium  } = data.url
   
       // console.log(data);
-      console.log(data.data);
+      console.log(data.price);
       // console.log(data.src);
   
       const html = `
       
-      <div class="col-md-4 ">
-      <div class="card mb-4 shadow-sm">
-        <img src="${data.img}" class="card-img-top img-fluid w-100" ' ' alt="...">
+      <div class="col-md-3">
+      <div class="card mb-3  shadow-sm">
+        <img src="${data.img}" class="card-img-top" alt="...">
         <div class="card-body">
-          <p class="card-text">
+          <h5 class="card-text">
             ${data.title}
-          </p>
-          <h2
+          </h5>
+          
+          <h6 class="catagory"> ${data.category} </h6>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
               <button type="button" class="btn btn-sm btn-outline-secondary">
-                View
+                Add to Cart
               </button>
               <button type="button" class="btn btn-sm btn-outline-secondary">
-                Edit
+                Ignore
               </button>
             </div>
-            <small class="text-muted">9 mins</small>
+             ${data.price} €
           </div>
         </div>
       </div>
@@ -48,7 +49,4 @@ const displayHTM = (datas) => {
       `;
       mainRow.insertAdjacentHTML("afterbegin", html);
     });
-  
-    // mainRow.insertAdjacentHTML('afterbegin',html )
   };
-  displayHTM()
