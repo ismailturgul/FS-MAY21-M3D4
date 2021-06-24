a108b4986e805a6e9c672b80506486c28ecc2d01
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-06-24 16:41:10 +0200

    Homework M3D4
    remove button  is working

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-const btnAddToCart = document.querySelector(".addToCart")
+const btnAddToCart = document.querySelector("#addToCart")
+console.log(btnAddToCart)
 const mainRow = document.querySelector(".main-row");
 const favRow = document.querySelector(".favorite.row")
 
 const getBooks = function(){
     fetch("https://striveschool-api.herokuapp.com/books").then(function(response){
-        console.log(response);
         return response.json();
     }).then(function(data){
-        console.log(data)
         displayHTM(data);
-    })
+    }) 
 };
 
 const displayHTM = (datas) => {
     datas.forEach((data) => {
-      // const img {medium  } = data.url
-  
-      // console.log(data);
-      console.log(data.price);
-      // console.log(data.src);
-  
       const html = `
       
-      <div class="col-md-3">
-      <div class="card mb-3  shadow-sm">
-        <img src="${data.img}" class="card-img-top" alt="...">
-        <div class="card-body">
+      <div class="col-md-3 booksPerColumn">
+      <div class="card mb-3 d-flex shadow-sm">
+        <img src="${data.img}" class="card-img img-fluid" alt="...">
+        <div class="card-body ">
           <h5 class="card-text">
             ${data.title}
           </h5>
@@ -33,10 +26,10 @@ const displayHTM = (datas) => {
           <h6 class="catagory"> ${data.category} </h6>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-secondary">
+              <button type="button" id="addToCart" class="btn btn-sm btn-outline-secondary">
                 Add to Cart
               </button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">
+              <button type="button" id="ignore" class="btn btn-sm btn-outline-secondary">
                 Ignore
               </button>
             </div>
@@ -48,5 +41,16 @@ const displayHTM = (datas) => {
       
       `;
       mainRow.insertAdjacentHTML("afterbegin", html);
+      document.getElementById("ignore").addEventListener('click', function(event) {
+    console.log("working")
+        let buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
     });
+    
+
+})
   };
+
+getBooks()
+let books = document.getElementsByClassName("booksPerColumn")
+
