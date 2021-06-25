db55ab5f3f42c81a805a8fac4f52417e2912c132
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-06-25 10:42:40 +0200

    shoppingcart

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const btnAddToCart = document.querySelector("#addToCart")
-console.log(btnAddToCart)
 const mainRow = document.querySelector(".main-row");
-const favRow = document.querySelector(".favorite.row")
+const favRow = document.querySelector(".favorite-row");
 
 const getBooks = function(){
     fetch("https://striveschool-api.herokuapp.com/books").then(function(response){
@@ -10,19 +8,17 @@ const getBooks = function(){
         displayHTM(data);
     }) 
 };
-
 const displayHTM = (datas) => {
     datas.forEach((data) => {
       const html = `
       
       <div class="col-md-3 booksPerColumn">
       <div class="card mb-3 d-flex shadow-sm">
-        <img src="${data.img}" class="card-img img-fluid" alt="...">
+        <img src="${data.img}" class="card-img alt=" d-flex...">
         <div class="card-body ">
           <h5 class="card-text">
             ${data.title}
           </h5>
-          
           <h6 class="catagory"> ${data.category} </h6>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
@@ -32,25 +28,25 @@ const displayHTM = (datas) => {
               <button type="button" id="ignore" class="btn btn-sm btn-outline-secondary">
                 Ignore
               </button>
-            </div>
+            </div class="price">
              ${data.price} €
           </div>
         </div>
       </div>
     </div>
       
-      `;
+      `;   
       mainRow.insertAdjacentHTML("afterbegin", html);
+
       document.getElementById("ignore").addEventListener('click', function(event) {
-    console.log("working")
         let buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+         buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+    }); 
+    // manipulate it to push it maybe with the displayHTM function but instead of mainRow -> favRow
+      document.getElementById("addToCart").addEventListener('click', function(event) {
+        let buttonClicked = event.target
+        let divToShoppingCart= buttonClicked.parentElement.parentElement.parentElement.parentElement = "background: green";
     });
-    
-
 })
-  };
-
+}
 getBooks()
-let books = document.getElementsByClassName("booksPerColumn")
-
